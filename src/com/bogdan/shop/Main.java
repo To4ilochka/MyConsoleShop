40106b0f009ee7675885763c1e1d0683b59f6d4b
To4ilochka <bogdan.goldenn@gmail.com>
2023-03-13 12:41:03 +0200

    Shop before .gitignore

--- src/com/bogdan/shop/Main.java
+++ src/com/bogdan/shop/Main.java
@@ -2,6 +2,7 @@ package com.bogdan.shop;
 
 import com.bogdan.shop.exception.IncorrectInputTextException;
 
+
 public class Main {
     public static void main(String[] args) {
         ShopConfiguration shopConfiguration = new ShopConfiguration();
